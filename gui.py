325458fa8b4e8076e88867cc9e4e8dfbbbb3f02d
Veronika <kika55cat@yandex.ru>
2017-05-20 23:22:09 +0300

    This programm have a error

--- gui.py
+++ gui.py
@@ -1,50 +1,46 @@
+import fasten
 import sys
-from PyQt5.QtWidgets import (QMainWindow, QTextEdit,
-    QAction, QFileDialog, QApplication)
+from PyQt5.QtWidgets import QApplication, QDesktopWidget, QWidget, QMainWindow, QAction
 from PyQt5.QtGui import QIcon
 
 
-class Example(QMainWindow):
+class FEWindow(QWidget):
 
     def __init__(self):
         super().__init__()
-
         self.initUI()
 
-
     def initUI(self):
+        
+        exitAction = QAction(QIcon('exit.png'), '&Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(app.quit)
 
-        self.textEdit = QTextEdit()
-        self.setCentralWidget(self.textEdit)
-        self.statusBar()
+        menubar = QMainWindow().menuBar()
+        file_menu = menubar.addMenu('&File')
+        file_menu.addAction(exitAction)
+        self.set_status_bar()
 
-        openFile = QAction(QIcon('open.png'), 'Open', self)
-        openFile.setShortcut('Ctrl+O')
-        openFile.setStatusTip('Open new File')
-        openFile.triggered.connect(self.showDialog)
-
-        menubar = self.menuBar()
-        fileMenu = menubar.addMenu('&File')
-        fileMenu.addAction(openFile)
-
-        self.setGeometry(300, 300, 350, 300)
-        self.setWindowTitle('File dialog')
+        self.resize(450, 300)
+        self.center()
+        self.setWindowTitle('Fast English: Английский для ленивых отличников')
+        self.setWindowIcon(QIcon('book.png'))
         self.show()
 
+    def center(self):
+        main_window = self.frameGeometry()
+        mon = QDesktopWidget().availableGeometry().center()
+        main_window.moveCenter(mon)
+        self.move(main_window.topLeft()) 
 
-    def showDialog(self):
-
-        fname = QFileDialog.getOpenFileName(self, 'Open file', '/home')[0]
-
-        f = open(fname, 'r')
-
-        with f:
-            data = f.read()
-            self.textEdit.setText(data)
+    def set_status_bar(self):
+        return QMainWindow().statusBar()        
 
 
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
-    ex = Example()
+
+    fe_window = FEWindow()
     sys.exit(app.exec_())
